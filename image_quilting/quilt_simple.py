15eefc18657decc96149ac46ea8bc2635520c5f9
haoxiangpeng <3150102255@zju.edu.cn>
2018-02-03 21:30:57 -0800

    :art: refactor cool

--- image_quilting/quilt_simple.py
+++ image_quilting/quilt_simple.py
@@ -1,52 +1,26 @@
 import argparse
-from PIL import Image, ImageChops
 import random
-from functools import wraps
-from time import time
-import numpy as np
+from image_quilting.Quilting import Quilt
 
 
-def timeit(f):
-  @wraps(f)
-  def wrapper(*args, **kwargs):
-    start = time()
-    result = f(*args, **kwargs)
-    end = time()
-    print('Elapsed time: {}'.format(end - start))
-    return result
+class QuiltSimple(Quilt):
+  def __init__(self, sample: str, outsize: int, patch_size: int):
+    Quilt.__init__(self, texture_path=sample, output_size=outsize, patch_size=patch_size)
+    self.patch_size = patch_size
   
-  return wrapper
-
-
-class QuiltSimple:
-  def __init__(self, sample: str, outsize: int, patchsize: int, overlap: int):
-    self.texture = Image.open(sample)
-    self.im = Image.new('RGB', size=(outsize, outsize))
-    self.patchsize = patchsize
-    self.outsize = outsize
-    self.overlap = overlap
-  
-  def patch_difference(self, rg1, rg2) -> int:
-    if rg1.size != rg2.size:
-      raise ArithmeticError('region size do not match')
-
-    difference = ImageChops.difference(rg1.convert('L'), rg2.convert('L'))
-    return sum(difference.getdata())
-  
-  @timeit
   def select_patch(self, region1=None, region2=None):
     smallest_diff = -1
     smallest_point = None
     count = 0
     while count < 1000:
       count += 1
-      random_i = random.randrange(self.overlap, self.texture.size[0] - self.patchsize)
-      random_j = random.randrange(self.overlap, self.texture.size[1] - self.patchsize)
-      random_region1 = self.texture.crop(
-        (random_i - self.overlap, random_j - self.overlap, self.patchsize + random_i, random_j)
+      random_i = random.randrange(self.overlap, self.texture_image.size[0] - self.patch_size)
+      random_j = random.randrange(self.overlap, self.texture_image.size[1] - self.patch_size)
+      random_region1 = self.texture_image.crop(
+        (random_i - self.overlap, random_j - self.overlap, self.patch_size + random_i, random_j)
       )
-      random_region2 = self.texture.crop(
-        (random_i - self.overlap, random_j, random_i, random_j + self.patchsize)
+      random_region2 = self.texture_image.crop(
+        (random_i - self.overlap, random_j, random_i, random_j + self.patch_size)
       )
       if region1 is None and region2 is None:
         smallest_point = (random_i, random_j)
@@ -60,31 +34,29 @@ class QuiltSimple:
       if smallest_diff < 0 or difference_sum < smallest_diff:
         smallest_diff = difference_sum
         smallest_point = (random_i, random_j)
-      # print(difference_sum)
-    return self.texture.crop(
-      (smallest_point[0], smallest_point[1], smallest_point[0] + self.patchsize, smallest_point[1] + self.patchsize)
+    return self.texture_image.crop(
+      (smallest_point[0], smallest_point[1], smallest_point[0] + self.patch_size, smallest_point[1] + self.patch_size)
     )
   
-  def quilt_simple(self) -> None:
-    for j in range(0, self.outsize, self.patchsize):
-      for i in range(0, self.outsize, self.patchsize):
-        region_up = None if j == 0 else self.im.crop((i - self.overlap, j - self.overlap, i + self.patchsize, j))
-        region_left = None if i == 0 else self.im.crop((i - self.overlap, j, i, j + self.patchsize))
+  def quilt_simple(self):
+    for j in range(0, self.output_size[0], self.patch_size):
+      for i in range(0, self.output_size[0], self.patch_size):
+        region_up = None if j == 0 else self.output_image.crop(
+          (i - self.overlap, j - self.overlap, i + self.patch_size, j))
+        region_left = None if i == 0 else self.output_image.crop((i - self.overlap, j, i, j + self.patch_size))
         texture_region = self.select_patch(
           region_up, region_left,
         )
-        self.im.paste(texture_region, (i, j, i + self.patchsize, j + self.patchsize))
-        print("==================", i, j)
-    return self.im
-    # print(result)
+        self.output_image.paste(texture_region, (i, j, i + self.patch_size, j + self.patch_size))
+    return self.output_image
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="quilt random function")
   parser.add_argument('-f', '--file', required=True, type=str, help='path to texture image')
   parser.add_argument('-o', '--outsize', required=True, type=int, help='output size')
-  parser.add_argument('-p', '--patchsize', required=True, type=int, help='patch size')
+  parser.add_argument('-p', '--patch_size', required=True, type=int, help='patch size')
   args = vars(parser.parse_args())
-  quilt_simple = QuiltSimple(args['file'], args['outsize'], args['patchsize'], args['patchsize'] / 3)
+  quilt_simple = QuiltSimple(args['file'], args['outsize'], args['patch_size'])
   result = quilt_simple.quilt_simple()
-  result.save(args['file'][:-4] + '_simple.jpg')
+  result.show()
--- image_quilting/random_sampled.py
+++ image_quilting/random_sampled.py
@@ -3,18 +3,15 @@ from PIL import Image
 import random
 import argparse
 
-ASSETS_PATH = '../assets/'
-TEXTURE_PATH = ASSETS_PATH + 'texture_redpot.png'
 
-
-def quilt_random(sample: str, outsize: int, patchsize: int) -> None:
+def quilt_random(sample: str, outsize: int, patch_size: int):
   texture = Image.open(sample)
   im = Image.new('RGB', size=(outsize, outsize))
-  for i in range(0, outsize, patchsize):
-    for j in range(0, outsize, patchsize):
-      random_shift = random.randrange(0, min(texture.size) - patchsize)
-      texture_region = texture.crop((random_shift, random_shift, patchsize + random_shift, patchsize + random_shift))
-      im.paste(texture_region, (i, j, i + patchsize, j + patchsize))
+  for i in range(0, outsize, patch_size):
+    for j in range(0, outsize, patch_size):
+      random_shift = random.randrange(0, min(texture.size) - patch_size)
+      texture_region = texture.crop((random_shift, random_shift, patch_size + random_shift, patch_size + random_shift))
+      im.paste(texture_region, (i, j, i + patch_size, j + patch_size))
   return im
 
 
@@ -22,7 +19,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="quilt random function")
   parser.add_argument('-f', '--file', required=True, type=str, help='path to texture image')
   parser.add_argument('-o', '--outsize', required=True, type=int, help='output size')
-  parser.add_argument('-p', '--patchsize', required=True, type=int, help='patch size')
+  parser.add_argument('-p', '--patch_size', required=True, type=int, help='patch size')
   args = vars(parser.parse_args())
-  result = quilt_random(args['file'], args['outsize'], args['patchsize'])
+  result = quilt_random(args['file'], args['outsize'], args['patch_size'])
   result.save(args['file'][:-4] + '_random.jpg')
